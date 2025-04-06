3681485c4dcb518452e6f20e5bc658d54dbacb58
Rudransh Bhardwaj <Rudranshbhardwaj2401@users.noreply.github.com>
2025-04-06 15:13:11 +0530

    Updating again

--- Lecture 02/Aritmetic_Operators.cpp
+++ Lecture 02/Aritmetic_Operators.cpp
@@ -1,13 +1,22 @@
+/* There are 5 Arithmetic Operators
++ Addition
+- Subtraction
+* Multiplication 
+/ Division
+% Modulus (returns remainder)
+*/
 #include<iostream>
 using namespace std;
 int main(){
 
-int a =1/2;
-cout<<a<<endl;
-int b =1/2.0;
-cout<<b<<endl;
-float c =1/2.0;
-cout<<c<<endl;
-cout<<1/2.0<<endl;
+int a = 2;
+int b = 3;
+cout<<a+b<<endl;  //5
+cout<<a-b<<endl;  //-1
+cout<<a*b<<endl;  //6
+cout<<a/b<<endl;  //int/int will be int. So 2/3=0.66=0
+cout<<b%a<<endl;  //1
+float b1=3.0;
+cout<<a/b1<<endl; //int/float=float. So 2/3.0=0.66
     return 0;
 }
--- Lecture 02/Readme.md
+++ Lecture 02/Readme.md
@@ -1,6 +1,6 @@
 # Lecture 02 - First Program, Basic Data Types and Operators
 ## First program
-https://github.com/Rudranshbhardwaj2401/DSA_with_CPP_by_Rudransh/blob/71f6888053048963093aa8941a45f1d32791beb6/Lecture%2002/First_Program.cpp
+https://github.com/Rudranshbhardwaj2401/DSA_with_CPP_by_Rudransh/blob/c458c854e7e0e8cd3c0d64f56b6f8b4723cf59d3/Lecture%2002/First_Program.cpp
 ## Data Types
 https://github.com/Rudranshbhardwaj2401/DSA_with_CPP_by_Rudransh/blob/5b3b1802d95f9875880c40ea46755abc3c8c0604/Lecture%2002/Data_Types.cpp
 ## Type Conversion
