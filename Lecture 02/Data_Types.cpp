065f4baadffbfd1f68d0277b1d257988d9b70d24
Rudransh Bhardwaj <Rudranshbhardwaj2401@users.noreply.github.com>
2025-04-06 14:34:19 +0530

    Update Data_Types.cpp

--- Lecture 02/Data_Types.cpp
+++ Lecture 02/Data_Types.cpp
@@ -1,13 +1,15 @@
+//We will see some Basic datatypes and their size on system
 #include <iostream>
 using namespace std;
 int main()
 {
-    int i = 5;
-    char c = 'A';
-    bool b = 1;
-    float f = 1.2;
-    double d = 1.23;
-    int s = sizeof(i);
+    bool b = 1;            //1byte or 8bits (but needs only 1bit)
+    char c = 'A';          //1byte or 8bits
+    int i = 5;             //4byte or 32bits
+    int s = sizeof(i);     //4byte or 32bits (as it is int)
+    float f = 1.2;         //4byte or 32bits
+    double d = 1.23;       //8byte or 64bits
+
     cout << i << endl;
     cout << c << endl;
     cout << b << endl;
